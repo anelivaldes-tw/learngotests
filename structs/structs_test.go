44983f55148ed2da5d79fc66ffbaddced0108f46
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-01-27 15:05:49 -0300

    anonymous struct and table driven tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -32,3 +32,24 @@ func TestShapes(t *testing.T) {
 		checkArea(t, circle, want)
 	})
 }
+
+//  "table driven tests"
+func TestArea(t *testing.T) {
+
+	// anonymous struct
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
+}
