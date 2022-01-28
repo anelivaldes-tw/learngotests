c71feafdaecb0d117f34a92c7f02219c560e4ac7
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-01-28 11:53:41 -0300

    using pointer (same memory address)

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -12,11 +12,16 @@ whatever we called the method from.(Are value receiver)*/
 
 // You can find out what the address of that bit of memory with &myVal.
 
-func (w Wallet) Deposit(amount int) {
+// The difference is the receiver type is *Wallet rather
+// than Wallet which you can read as "a pointer to a wallet"
+
+// Now the memory address are the same
+
+func (w *Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	fmt.Printf("address of balance in Balance is %v \n", &w.balance)
-	return w.balance
+	return (*w).balance // It is not required to change to (*w), but better for consistency
 }
