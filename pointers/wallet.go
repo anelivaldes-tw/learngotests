7e2242ebc6c12efe68456b11bb444d4563c60011
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-01-28 11:49:40 -0300

    value receiver are copied (different memory address)

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,5 +1,7 @@
 package pointers
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
@@ -8,9 +10,13 @@ type Wallet struct {
 When calling func (w Wallet) Deposit(amount int) the w is a copy of
 whatever we called the method from.(Are value receiver)*/
 
+// You can find out what the address of that bit of memory with &myVal.
+
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 func (w Wallet) Balance() int {
+	fmt.Printf("address of balance in Balance is %v \n", &w.balance)
 	return w.balance
 }
